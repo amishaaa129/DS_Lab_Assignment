051034e22a4f0543abdcd7fe1bccb35d2983ff49
amishaaa129 <amishawilliams2019@gmail.com>
2025-11-27 10:32:08 +0530

    assignment 9 done

---
+++ assignment9/q2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void DFSfunc(int node, vector<vector<int>>& adj, vector<bool>& visited) {
+    visited[node] = true;
+    cout << node << " ";
+    for (int neighbor : adj[node])
+        if (!visited[neighbor])
+            DFSfunc(neighbor, adj, visited);
+}
+void DFS(int start, vector<vector<int>>& adj, int n) {
+    vector<bool> visited(n, false);
+    DFSfunc(start, adj, visited);
+}
+
+int main() {
+    int n = 5;
+    vector<vector<int>> adj(n);
+    adj[0] = {1, 2};
+    adj[1] = {0, 3};
+    adj[2] = {0, 4};
+    adj[3] = {1};
+    adj[4] = {2};
+
+    cout << "DFS starting from node 0: ";
+    DFS(0, adj, n);
+}
